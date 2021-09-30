f4066638c20e77ba2cfaa43c54c8ba1efffed5ce
Kiran <kiranraoaluve@gmail.com>
2021-09-30 15:41:30 +0530

    Object file created

--- Input.cpp
+++ Input.cpp
@@ -1,16 +1,45 @@
 #include<bits/stdc++.h>
 #include "Input.h"
+#include <fstream>
+
 using namespace std;
 
 vector<pair<pair<string,string>,int>> Input::CLI(){
  int n;
- cout<<"Enter the Edges"<<endl;
- cin>>n;
- for(int i=0;i<n;i++){
-   cin>>name1>>name2>>distance;
-   input.push_back(make_pair(make_pair(name1,name2),distance));
- }
- cout<<"Enter the destination"<<endl;
+
+// cout<<"Enter the no of edges"<<endl;
+// cin>>n;
+// for(int i=0;i<n;i++){
+//   cin>>name1>>name2>>distance;
+//   input.push_back(make_pair(make_pair(name1,name2),distance));
+// }
+
+  ifstream file("input.txt");
+
+  int weight, edgecounter=0;
+  string distancestr, data = "";
+
+  while(!file.eof())
+  {
+      getline(file, data,',');
+      name1=data;
+      getline(file, data,',');
+      name2=data;
+      getline(file, data,'\n');
+      distancestr=data;
+      distance = stoi(distancestr);
+      edgecounter++;
+      input.push_back(make_pair(make_pair(name1,name2),distance));
+  }
+
+  file.close();
+  n=edgecounter/2;
+
+
+
+
+
+ cout<<"Enter the Source"<<endl;
  cin>>source;
  cout<<"Enter the Destination"<<endl;
  cin>>destination;
